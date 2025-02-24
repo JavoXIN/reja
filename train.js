c7241c4c744e4a41c0a24d129468539893d22ffd
Javoxir <javohir.toshtemirov123@gmail.com>
2025-02-24 19:48:44 +0900

    feat: adding MITask solve to train.js

--- train.js
+++ train.js
@@ -123,3 +123,20 @@ run();
 
 //=============================================================
 
+//============================Mitask 1=========================
+
+function countLetter(letter, word) {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) 
+    {
+      if (word[i] === letter) 
+        {
+          count++;
+        }
+    }
+    return count;
+  }
+  
+console.log(countLetter("a", "mehanizalashtirilganligidan-a?")); 
+ 
+// Result: 6
